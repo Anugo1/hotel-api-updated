edc1d5d252a981c5dfb771f52b06ef585cce55c9
Anugo1 <ekehumphery@gmail.com>
2023-02-20 23:32:35 +0100

    auth commit

---
+++ source/middleware authenticators/authentication.js
@@ -0,0 +1,11 @@
+const authorize = async (req, res, next) => {
+    if (!req.user.isAdmin) {
+        return res.status(403).send({
+          success: false,
+          message: "Access denied"
+        })
+      }
+      next();
+    };
+    
+    module.exports = authorize;   
